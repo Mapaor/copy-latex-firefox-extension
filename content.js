17c4c60749bc1d95e9598b270ceebb6ee5558aa0
Mapaor4 <mpardoor24@alumnes.ub.edu>
2026-01-08 15:31:10 +0100

    dark mode and check icon svg

--- content.js
+++ content.js
@@ -64,6 +64,10 @@ window.addEventListener('message', function(event) {
 let overlay;
 let currentTarget = null;
 
+const copy_svg = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4 a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>';
+const check_svg = '<svg class="check-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 -1 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"><path d="M20 5 9 17l-5-5"/></svg>';
+
+
 function isWikipedia() {
   const hostname = window.location.hostname;
   return hostname.endsWith('.wikipedia.org') || hostname === 'www.wikiwand.com' || hostname === "wikimedia.org" || hostname.endsWith(".wikiversity.org") || hostname.endsWith(".wikibooks.org");
@@ -191,13 +195,7 @@ function createOverlay() {
 
   // HTML overlay content with inline SVG icon and 'Click to copy' text
   overlay.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" 
-         viewBox="0 0 24 24" fill="none" stroke="currentColor" 
-         stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
-      <path d="M5 15H4a2 2 0 0 1-2-2V4
-               a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
-    </svg>
+    ${copy_svg}
     <span>Click to copy</span>
   `;
 
@@ -228,10 +226,10 @@ function hideOverlay() {
 function copyLatex(tex) {
   navigator.clipboard.writeText(tex).then(() => {
     overlay.classList.add('copied');
-    overlay.querySelector('span').textContent = 'Copied! ✅'; // todo: replace emoji with inline svg tw icon: https://mapaor.github.io/tw-emojis/tw-emojis-svgs/2705.svg
+    overlay.querySelector('span').innerHTML = `Copied! ${check_svg}`;
     setTimeout(() => {
       overlay.classList.remove('copied');
-      overlay.querySelector('span').textContent = 'Click to copy';
+      overlay.querySelector('span').innerHTML = 'Click to copy';
     }, 1500);
   }).catch(err => {
     console.error("[HoverLatex] Clipboard error:", err);
